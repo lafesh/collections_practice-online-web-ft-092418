df55a242d67b655ecc6ffc3242a61d3c1dceea05
Gina Fedder <ginafedder@yahoo.com>
2018-09-25 17:53:21 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,11 +1,15 @@
 def sort_array_asc(array)
-  return array.sort
+  array.sort
 end 
 
 def sort_array_desc(array)
-  return array.sort {|x, y| y <=> x} 
+  array.sort {|x, y| y <=> x} 
 end 
 
 def sort_array_char_count(array)
   array.sort {|x, y| x.length <=> y.length}
+end 
+
+def swap_elements(array)
+  
 end 
